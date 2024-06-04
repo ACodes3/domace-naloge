c206b85a5e4713972aec0c26102d50b169f56d41
Aspasija Cvetkoska <146888700+ACodes3@users.noreply.github.com>
2024-06-04 22:41:54 +0200

    Update menu.php

---
+++ domaco-nalogo/tpl/menu.php
@@ -0,0 +1,16 @@
+<?php
+$links = [
+    [
+        'link' => 'index.html', //ali index.php
+        'title' => 'Domov'
+    ],
+    [
+        'link' => 'o-meni.html', //ali o-meni.php
+        'title' => 'O meni'
+    ],
+    [
+        'link' => 'kontakt.html', //ali kontakt.php
+        'title' => 'Kontakt'
+    ],
+];
+?>
